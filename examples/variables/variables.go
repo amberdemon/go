fb4a13dc5519ad1453d76e742e8ccaf0dc9c8d4b
Warren Green <warren.green84@gmail.com>
2019-04-25 20:11:18 +0100

    restructuring go examples

--- examples/variables/variables.go
+++ examples/variables/variables.go
@@ -5,10 +5,17 @@ import (
 )
 
 func main() {
+	var i, j int = 1, 2
+	fmt.Printf("i is %d and j is %d\n", i, j)
+
 	course := "docker course"
 	fmt.Println("course is ", course)
 	changeCourse(&course)
 	fmt.Println("course is now ", course)
+
+	const noChange string = "I cant change"
+	fmt.Println(noChange)
+	// noChange = "oh no"
 }
 
 func changeCourse(course *string) string {
